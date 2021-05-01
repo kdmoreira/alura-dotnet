db9c5f764e679ac12dacb2828d9b74978e01c4c2
Karina Moreira <kakatinosa@hotmail.com>
2021-05-01 14:26:28 -0300

    Add packages

--- CS05-ByteBank.SistemaAgencia/Program.cs
+++ CS05-ByteBank.SistemaAgencia/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Humanizer;
 
 using ByteBank.Modelos;
 
@@ -13,7 +14,30 @@ namespace ByteBank.SistemaAgencia
         static void Main(string[] args)
         {
             ContaCorrente conta = new ContaCorrente(1, 1);
+
             conta.Sacar(10);
+
+            DateTime dataPagamento = new DateTime(2021, 10, 18);
+            Console.WriteLine(dataPagamento);
+
+            DateTime dataCorrente = DateTime.Now;
+            Console.WriteLine(dataCorrente);
+
+            TimeSpan diferenca = dataPagamento - dataCorrente;
+            Console.WriteLine(diferenca); // Não muito inteligível, para isto, existe o Humanizer
+
+            // Ainda não é o ideal
+            Console.WriteLine(GetIntervaloTempoLegivel(diferenca));
+
+            // Após a instalação do pacote NuGet Humanizer
+            string mensagem = "Vencimento em " + TimeSpanHumanizeExtensions.Humanize(diferenca);
+            Console.WriteLine(mensagem);
+        }
+
+        // Poderia adaptar para semanas, meses, mas o Humanizer é uma forma melhor
+        static string GetIntervaloTempoLegivel(TimeSpan timeSpan)
+        {
+            return timeSpan.Days + " dias";
         }
     }
 }
