5c8b24a5fd1cc4bdbf9ddcf8d20582b0e86aec71
Karina Moreira <kakatinosa@hotmail.com>
2021-04-30 23:08:00 -0300

    Add files

---
+++ CS05-ByteBank.SistemaAgencia/Estagiario.cs
@@ -0,0 +1,28 @@
+﻿using ByteBank.Modelos.Funcionarios;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharp_02
+{
+    public class Estagiario : Funcionario
+    {
+        public Estagiario(double salario, string cpf) : base (salario, cpf)
+        {
+
+        }
+
+        public override void AumentarSalario()
+        {
+            throw new NotImplementedException();
+        }
+
+        // Na classe funcionário, este método é internal protected, porém, como
+        // Estagiário faz parte de outro projeto, deve ser somente protected, pois
+        // não faz sentido que seja internal, ou seja, visível dentro deste projeto também
+        protected override double GetBonificacao()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CS05-ByteBank.SistemaAgencia/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using ByteBank.Modelos;
+
+namespace ByteBank.SistemaAgencia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente conta = new ContaCorrente(1, 1);
+            conta.Sacar(10);
+        }
+    }
+}
